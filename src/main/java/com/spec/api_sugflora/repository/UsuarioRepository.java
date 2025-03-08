47b3d905f2934648f8cd2ae5a52e94758b191ca1
VG-Correa <v.victorgabriel2014@gmail.com>
2025-03-08 11:30:11 -0300

    .

--- src/main/java/com/spec/api_sugflora/repository/UsuarioRepository.java
+++ src/main/java/com/spec/api_sugflora/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.spec.api_sugflora.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,11 @@ import com.spec.api_sugflora.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
-    Usuario findByUsername(String username);
+    Optional<Usuario> findByUsername(String username);
+
+    Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByCpf(String cpf);
+
+    Optional<Usuario> findByRg(String rg);
 }
--- src/main/java/com/spec/api_sugflora/service/UsuarioService.java
+++ src/main/java/com/spec/api_sugflora/service/UsuarioService.java
@@ -1,17 +1,76 @@
 package com.spec.api_sugflora.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.relational.core.sql.IsNull;
 import org.springframework.stereotype.Service;
 
 import com.spec.api_sugflora.model.Usuario;
 import com.spec.api_sugflora.repository.UsuarioRepository;
+
+import jakarta.persistence.EntityExistsException;
 
 @Service
 public class UsuarioService {
     @Autowired
     UsuarioRepository usuarioRepository;
     
+    public Usuario findByEmail(String email) {
+        Usuario usuario = usuarioRepository.findByEmail(email).orElse(null);
+        return usuario;
+    }
+
+    public boolean userExistsByEmail(String email) {
+        boolean exist = findByEmail(email) != null ?  true :  false;
+        return exist;
+    }
+
+    public Usuario findByUsername(String username) {
+        Usuario usuario = usuarioRepository.findByUsername(username).orElse(null);
+        return usuario;
+    }
+
+    public boolean userExistsByUsername(String username) {
+        boolean exist = findByUsername(username) != null ? true : false;
+        return exist;
+    }
+
+    public Usuario findByCpf(String cpf) {
+        Usuario usuario = usuarioRepository.findByCpf(cpf).orElse(null);
+        return usuario;
+    }
+
+    public boolean userExistsByCPF(String cpf) {
+        boolean exist = findByCpf(cpf) != null ? true : false;
+        return exist;
+    }
+
+    public Usuario findByRg(String rg) {
+        Usuario usuario = usuarioRepository.findByRg(rg).orElse(null);
+        return usuario;
+    }
+
+    public boolean userExistsByRg(String rg) {
+        boolean exist = findByRg(rg) != null ? true : false;
+        return exist;
+    }
+
+    public void isValid(Usuario usuario) {
+        if (usuario.getCpf() == null) {
+            
+        }
+    }
+
     public Usuario save(Usuario user) {
+        if (userExistsByEmail(user.getEmail())) {
+            throw new EntityExistsException("Este e-mail já está cadastrado");
+        } else if (userExistsByUsername(user.getUsername())) {
+            throw new EntityExistsException("Já existe um usuário com este username");
+        } else if (userExistsByCPF(user.getCpf())) {
+            throw new EntityExistsException("Já existe um usuário com este CPF");
+        } else if (userExistsByRg(user.getRg())) {
+            throw new EntityExistsException("Já existe um usuário com este RG");
+        }
+
         Usuario novoUsuario = usuarioRepository.save(user);
         return novoUsuario;
     }
