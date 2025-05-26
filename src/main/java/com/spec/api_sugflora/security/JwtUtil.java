16c2cf5529043ef052228d4432041c5b6ac4f27c
VG-Correa <v.victorgabriel2014@gmail.com>
2025-05-26 19:21:19 -0300

    .

--- src/main/java/com/spec/api_sugflora/security/JwtUtil.java
+++ src/main/java/com/spec/api_sugflora/security/JwtUtil.java
@@ -26,7 +26,8 @@ import javax.crypto.spec.SecretKeySpec;
 public class JwtUtil {
 
     private static final String SECRET_KEY = "minha-chave-secreta-muito-forte-e-grande-para-jwt-1234567890";
-    private static final long EXPIRATION_TIME = 60000;
+    private static final long MINUTES = 10;
+    private static final long EXPIRATION_TIME = MINUTES * 60000;
 
     private Key key(){
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
